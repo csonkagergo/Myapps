3951275b1704c25e92c11db90fe3ae19a46aa53e
csonkagergo <csonka.gergo7@gmail.com>
2019-03-13 12:04:55 +0100

    #4:ciklusok

--- esf/esf/Exercise.cs
+++ esf/esf/Exercise.cs
@@ -8,18 +8,30 @@ namespace esf
 {
     class Exercise
     {
+        int v = 0;
 
         ///adattagok létrehozása:public,private,protedted
         /// public :nyilvános elérés
         /// private: csak az osztály számára látszik
-       
-       
+
+
         public string Name;
 
+        //konstruktor: inicializálja az adattagjait
         public Exercise(string aName)
         {
-            Name = aName;
 
+            Name = aName;
+        }
+        public void PrintName()
+        {
+            Console.WriteLine(Name);
+        }
+
+
+        public string getName()
+        {
+            return Name;
         }
 
     }
--- esf/esf/Program.cs
+++ esf/esf/Program.cs
@@ -10,17 +10,39 @@ namespace esf
     {
         static void Main(string[] args)
         {
-
+            int v = 0;
             Exercise ex = new Exercise("Csonka Gergő");
 
             //ex.Name = "Csonka Gergő";
             ///
 
 
-            ///
+            ex.PrintName();
 
 
-            Console.WriteLine(ex.Name);
+            Console.WriteLine(ex.getName());
+            ///ciklusok
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("ciklus:<váltoizó hivatkozás I.>" + i);
+                Console.WriteLine("ciklus:<Változó hivatkozás II.>{0}", i);
+            }
+            v = 2;
+            while(v>0)
+            {
+                Console.WriteLine("while ciklus");
+                v--;
+                   
+            }
+            do
+            {
+
+                Console.WriteLine("Do while ciklus");
+                v--;
+            } while (v > 0);
+
+
 
             Console.ReadKey();
 

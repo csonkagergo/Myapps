53421f085b618380f7abd6d8548e25f87165fc35
csonkagergo <csonka.gergo7@gmail.com>
2019-04-10 11:19:29 +0200

    #6: object

--- TipusKonverzio/TipusKonverzio/Program.cs
+++ TipusKonverzio/TipusKonverzio/Program.cs
@@ -14,20 +14,29 @@ namespace TipusKonverzio
            // int x = 10;
            //if (x is int )//ha x egy int 
             {
-                Console.WriteLine("x tipusa int");
+               // Console.WriteLine("x tipusa int");
             }
             object a = "123";
             object b = "Hello";
             object c = 10;
 
-            string aa = a as int;
-            Console.WriteLine(aa == null ? "NULL" : aa); // 123 
+            string aa = a as string;
+                //expr1 ?v1 : v2;
+             Console.WriteLine(aa == null ? "NULL" : aa); // 123 
 
             string bb = b as string;
             Console.WriteLine(bb == null ? "NULL" : bb); // Hello 
 
             string cc = c as string;
             Console.WriteLine(cc == null ? "NULL" : cc); // NULL 
+
+            int x = 5;
+            Console.WriteLine(x ==5 ? "x==5" : "x !=5");
+
+            for (char ch = 'a';ch <= 'z'; ++ch)
+            {
+                Console.Write(int)ch + "-> " (int)ch + " ");
+            }
 
             Console.ReadKey();
         }

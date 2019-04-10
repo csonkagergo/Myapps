07bf10cfa4f0bdd7b84797fed625ec00c8385455
csonkagergo <csonka.gergo7@gmail.com>
2019-04-10 12:12:54 +0200

    #6: tobb dimenzios tomb

--- TipusKonverzio/TipusKonverzio/Program.cs
+++ TipusKonverzio/TipusKonverzio/Program.cs
@@ -11,18 +11,18 @@ namespace TipusKonverzio
         static void Main(string[] args)
         {
             //Is operátor kiprobálása
-           // int x = 10;
-           //if (x is int )//ha x egy int 
+            // int x = 10;
+            //if (x is int )//ha x egy int 
             {
-               // Console.WriteLine("x tipusa int");
+                // Console.WriteLine("x tipusa int");
             }
             object a = "123";
             object b = "Hello";
             object c = 10;
 
             string aa = a as string;
-                //expr1 ?v1 : v2;
-             Console.WriteLine(aa == null ? "NULL" : aa); // 123 
+            //expr1 ?v1 : v2;
+            Console.WriteLine(aa == null ? "NULL" : aa); // 123 
 
             string bb = b as string;
             Console.WriteLine(bb == null ? "NULL" : bb); // Hello 
@@ -31,9 +31,9 @@ namespace TipusKonverzio
             Console.WriteLine(cc == null ? "NULL" : cc); // NULL 
 
             int x = 5;
-            Console.WriteLine(x ==5 ? "x==5" : "x !=5");
+            Console.WriteLine(x == 5 ? "x==5" : "x !=5");
 
-            for (char ch = 'a';ch <= 'z'; ++ch)
+            for (char ch = 'a'; ch <= 'z'; ++ch)
             {
                 //Console.Write(int)ch + "-> " (int)ch + " ");
             }
@@ -43,18 +43,18 @@ namespace TipusKonverzio
             Random r = new Random();
             for (int i = 0; i < iTomb.Length; ++i)
             {
-                Console.Write("kérem az {0}. számot",i);
+                Console.Write("kérem az {0}. számot", i);
                 //Console.Write("kérem az" + i +". számot", i);
                 // A kivétel keletkezési helye(exception)
                 try
                 {
                     iTomb[i] = int.Parse(Console.ReadLine());
-                    iPrev = i;
+                   // iPrev = i;
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine("hiba." + e.Message);
-                    i = iPrev;
+                   // i = iPrev;
                 }
                 // iTomb[i] = r.Next();
                 //konzolról kérje be a számot
@@ -68,6 +68,14 @@ namespace TipusKonverzio
             }
 
             //tömb kezdeti értékének megadása
+            int[] iArr = new int[]{1,2,3,4,};
+            string[] sArr = new string[] { "banan","mango","alma","korte" };
+
+            Array.Sort(sArr);
+            for (int i=0;i< iArr.Length)
+
+                //tobb dimenzios tomb
+
 
                 Console.ReadKey();
         }
